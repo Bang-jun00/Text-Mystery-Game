1ca9e574ceafeecad06ea75919eac90fdb0e938b
Bang-jun00 <qudwns5250@naver.com>
2025-04-01 16:40:14 +0900

    [Feat]간단한 기획

--- Text-Mystery-Game/Text-Mystery-Game/Program.cs
+++ Text-Mystery-Game/Text-Mystery-Game/Program.cs
@@ -1,1 +1,18 @@
 ﻿namespace Text_Mystery_Game
+{
+    internal class Program
+    {
+        // Text 추리게임
+        // 정답을 선택해야하고, 오답시 3번까지는 재도전 가능
+        // 3번을 초과하면 Game Over
+        // 1~6까지 주사위 굴려서 힌트갯수 랜덤
+        // 힌트를 담을 수 있는 배열
+        // 힌트 사용시 힌트 나오게 출력
+        static void Main(string[] args)
+        {
+            Game.GameStart();
+            Game.GameProgress();
+            Game.GameStop(); 
+        }
+    }
+}
